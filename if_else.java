0bf23caf1a2d1ef8c727cb35c18cc2cfe4a4638d
Krishna Kotgire <krishnakotgire12369@gmail.com>
2023-03-14 16:47:42 +0530

    If else program in java

---
+++ if_else.java
@@ -0,0 +1,11 @@
+class if_else{
+    public static void main(String [] args){
+        int a=10;
+        if( a > 10)
+        {
+            System.out.println("A i greater than 10");
+        }else{
+          System.out.println("a less than 10 or equal to 10");
+        }
+    }
+}
