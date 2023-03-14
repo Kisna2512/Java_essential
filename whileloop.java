a772242225829eb4963d98232a331f982a04f306
Krishna Kotgire <krishnakotgire12369@gmail.com>
2023-03-14 17:28:36 +0530

    While loop in java

---
+++ whileloop.java
@@ -0,0 +1,9 @@
+public class whileloop {
+    public static void main(String[] args) {
+        int x = 10;
+        while (x < 20) {
+            System.out.println(x);
+            x = x + 1;
+        }
+    }
+}
