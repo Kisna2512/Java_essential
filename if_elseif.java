487a213d326213444f5f0d34adfe33e6c2b4ebde
Krishna Kotgire <krishnakotgire12369@gmail.com>
2023-03-14 17:25:53 +0530

    if else ladder

---
+++ if_elseif.java
@@ -0,0 +1,12 @@
+class if_elseif {
+    public static void main(String[] args) {
+        int a = 4;
+        if (a == 4) {
+            System.out.println("a is equal to 4");
+        } else if (a == 5) {
+            System.out.println("a is equal to 5");
+        } else {
+            System.out.println("a is diffrent");
+        }
+    }
+}
